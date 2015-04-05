34ecc0c72c146681b2705868b492a79ac3284109
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-04-04 19:23:30 -0500

    Added jade support to Gulp + less sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ var gulp = require('gulp')
     , rename = require('gulp-rename')
     , minifycss = require('gulp-minify-css')
     , minifyhtml = require('gulp-minify-html')
-    , processhtml = require('gulp-processhtml')
     , jshint = require('gulp-jshint')
     , streamify = require('gulp-streamify')
     , uglify = require('gulp-uglify')
     , less = require('gulp-less')
+    , jade = require('gulp-jade')
+    , sourcemaps = require('gulp-sourcemaps')
     , connect = require('gulp-connect')
     , source = require('vinyl-source-stream')
     , browserify = require('browserify')
@@ -23,108 +24,108 @@ var gulp = require('gulp')
 var watching = false;
 
 paths = {
-  assets: 'src/assets/**/*',
-  less:    'src/less/*.less',
-  libs:   [
-    './src/bower_components/phaser-official/build/phaser.js'
-  ],
-  js:     ['src/js/*.js', 'src/js/**/*.js'],
-  entry: './src/js/main.js',
-  dist:   './dist/'
+    assets: 'src/assets/**/*',
+    less:    'src/less/*.less',
+    libs: [
+        './src/bower_components/phaser-official/build/phaser.js'
+    ],
+    jade: ['src/index.jade', 'src/tmpl/*.jade', 'src/tmpl/**/*.jade'],
+    js: ['src/js/*.js', 'src/js/**/*.js'],
+    entry: './src/js/main.js',
+    dist: './dist/'
 };
 
 gulp.task('clean', function () {
-  return gulp.src(paths.dist)
-      .pipe(vinylPaths(del))
-      .on('error', gutil.log);
+    return gulp.src(paths.dist)
+        .pipe(vinylPaths(del))
+        .on('error', gutil.log);
 });
 
 gulp.task('copy', ['clean'], function () {
-  gulp.src(paths.assets)
-      .pipe(gulp.dest(paths.dist + 'assets'))
-      .on('error', gutil.log);
+    gulp.src(paths.assets)
+        .pipe(gulp.dest(paths.dist + 'assets'))
+        .on('error', gutil.log);
 });
 
 gulp.task('copylibs', ['clean'], function () {
-  gulp.src(paths.libs)
-      .pipe(gulpif(!watching, uglify({outSourceMaps: false})))
-      .pipe(gulp.dest(paths.dist + 'js/lib'))
-      .on('error', gutil.log);
+    gulp.src(paths.libs)
+        .pipe(gulpif(!watching, uglify({outSourceMaps: false})))
+        .pipe(gulp.dest(paths.dist + 'js/lib'))
+        .on('error', gutil.log);
 });
 
 gulp.task('compilejs', ['clean'], function () {
-  var bundler = browserify({
-    cache: {}, packageCache: {}, fullPaths: true,
-    entries: [paths.entry],
-    debug: watching
-  })
-      .transform(babelify);
-  if(watching) bundler.plugin(errorify);
+    var bundler = browserify({
+        cache: {}, packageCache: {}, fullPaths: true,
+        entries: [paths.entry],
+        debug: watching
+    })
+        .transform(babelify);
+    if(watching) bundler.plugin(errorify);
 
-  var bundlee = function() {
-    return bundler
-        .bundle()
-        .pipe(source('main.min.js'))
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-        .pipe(gulpif(!watching, streamify(uglify({outSourceMaps: false}))))
-        .pipe(gulp.dest(paths.dist))
-        .on('error', gutil.log);
-  };
+    var bundlee = function() {
+        return bundler
+            .bundle()
+            .pipe(source('js/main.min.js'))
+            .pipe(jshint('.jshintrc'))
+            .pipe(jshint.reporter('default'))
+            .pipe(gulpif(!watching, streamify(uglify({outSourceMaps: false}))))
+            .pipe(gulp.dest(paths.dist))
+            .on('error', gutil.log);
+    };
 
-  if (watching) {
-    bundler = watchify(bundler);
-    bundler.on('update', bundlee)
-  }
+    if (watching) {
+        bundler = watchify(bundler);
+        bundler.on('update', bundlee)
+    }
 
-  return bundlee();
+    return bundlee();
 });
 
 gulp.task('compileless', ['clean'], function () {
-  gulp.src(paths.less)
-      .pipe(less())
-      .pipe(concat('main.css'))
-      .pipe(gulpif(!watching, minifycss({
-        keepSpecialComments: false,
-        removeEmpty: true
-      })))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(paths.dist))
-      .on('error', gutil.log);
+    gulp.src(paths.less)
+        .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(concat('css/main.css'))
+            .pipe(gulpif(!watching, minifycss({
+                keepSpecialComments: false,
+                removeEmpty: true
+            })))
+            .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.dist))
+        .on('error', gutil.log);
 });
 
-gulp.task('processhtml', ['clean'], function() {
-  return gulp.src('src/index.html')
-      .pipe(processhtml())
-      .pipe(gulp.dest(paths.dist))
-      .on('error', gutil.log);
-});
-
-gulp.task('minifyhtml', ['processhtml'], function() {
-  gulp.src('dist/index.html')
-      .pipe(gulpif(!watching, minifyhtml()))
-      .pipe(gulp.dest(paths.dist))
-      .on('error', gutil.log);
+gulp.task('compilejade', ['clean'], function() {
+    gulp.src(paths.jade)
+        .pipe(jade({
+            pretty: watching
+        }))
+        .pipe(gulpif(!watching, minifyhtml()))
+        .pipe(gulp.dest(paths.dist))
+        .on('error', gutil.log);
 });
 
 gulp.task('html', ['build'], function(){
-  gulp.src('dist/*.html')
-      .pipe(connect.reload())
-      .on('error', gutil.log);
+    gulp.src('dist/*.html')
+        .pipe(connect.reload())
+        .on('error', gutil.log);
 });
 
 gulp.task('connect', function () {
-  connect.server({
-    root: ['./dist'],
-    port: 8000,
-    livereload: true
-  });
+    connect.server({
+        root: ['./dist'],
+        port: 8000,
+        livereload: true
+    });
 });
 
 gulp.task('watch', function () {
-  watching = true;
-  return gulp.watch(['./src/index.html', paths.css, paths.js], ['build', 'html']);
+    watching = true;
+    return gulp.watch([paths.less, paths.jade, paths.js], ['build', 'html']);
 });
 
 gulp.task('default', ['connect', 'watch', 'build']);
-gulp.task('build', ['clean', 'copy', 'copylibs', 'compilejs', 'compileless', 'processhtml', 'minifyhtml']);
+gulp.task('build', ['clean', 'copy', 'copylibs', 'compile']);
+gulp.task('compile', ['compilejs', 'compileless', 'compilejade'])

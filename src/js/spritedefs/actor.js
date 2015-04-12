eab30e27580f9476f9ec21487aea028355c4b5bb
Kyle Kemp <kyle@seiyria.com>
2015-04-12 11:58:38 -0500

    added sprite definitions for randomly generating characters

---
+++ src/js/spritedefs/actor.js
@@ -0,0 +1,170 @@
+module.exports = [
+
+  // Actor1
+  {
+    startPosition: 0,
+    gender: 'male',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 3,
+    gender: 'female',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 6,
+    gender: 'male',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 9,
+    gender: 'female',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 48,
+    gender: 'female',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 51,
+    gender: 'female',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 54,
+    gender: 'male',
+    file: 'Actor1'
+  },
+  {
+    startPosition: 57,
+    gender: 'male',
+    file: 'Actor1'
+  },
+
+  // Actor2
+  {
+    startPosition: 0,
+    gender: 'female',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 3,
+    gender: 'female',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 6,
+    gender: 'male',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 9,
+    gender: 'female',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 48,
+    gender: 'male',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 51,
+    gender: 'female',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 54,
+    gender: 'male',
+    file: 'Actor2'
+  },
+  {
+    startPosition: 57,
+    gender: 'female',
+    file: 'Actor2'
+  },
+
+  // Actor3
+  {
+    startPosition: 0,
+    gender: 'male',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 3,
+    gender: 'female',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 6,
+    gender: 'male',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 9,
+    gender: 'female',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 48,
+    gender: 'male',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 51,
+    gender: 'female',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 54,
+    gender: 'male',
+    file: 'Actor3'
+  },
+  {
+    startPosition: 57,
+    gender: 'female',
+    file: 'Actor3'
+  },
+
+  // Actor4
+  {
+    startPosition: 0,
+    gender: 'male',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 3,
+    gender: 'female',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 6,
+    gender: 'male',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 9,
+    gender: 'female',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 48,
+    gender: 'male',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 51,
+    gender: 'female',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 54,
+    gender: 'male',
+    file: 'Actor4'
+  },
+  {
+    startPosition: 57,
+    gender: 'female',
+    file: 'Actor4'
+  }
+];

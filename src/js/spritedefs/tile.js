03d5b4c45a8319a1cb6a6b2b72d874460f39a1a7
Kyle Kemp <kyle@seiyria.com>
2015-04-19 18:50:37 -0500

    start autotile attempt

---
+++ src/js/spritedefs/tile.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  mountain: {
+    gid: 798,
+    sheet: 'World_A2',
+    next: 32
+  }
+
+};
--- src/js/states/game.js
+++ src/js/states/game.js
@@ -1,5 +1,6 @@
 var Player = require('../entities/player');
 var MapGenerator = require('../generators/scenariomap');
+var TileDef = require('../spritedefs/tile');
 
 var pregens = {
   openworld1: {
@@ -40,7 +41,15 @@ var Game = function () {
 
 module.exports = Game;
 
-var map = new MapGenerator(_.extend(pregens.openworld1, {worldWidth: 24, worldHeight: 24, distanceReq: {keep: 5}}));
+var map = new MapGenerator(_.extend(pregens.openworld1, {worldWidth: 30, worldHeight: 30, distanceReq: {keep: 5}}));
+
+var numToSprite = function(map, x, y, num, width = 32) {
+  switch(num) {
+    case 3: return (width*6)-2;
+    case 6: return (width*3)-2;
+    case 7: return
+  }
+};
 
 Game.prototype = {
 
@@ -58,24 +67,23 @@ Game.prototype = {
     terrainLayer.resizeWorld();
 
     var collisionLayerObj = tilemap.createBlankLayer('collision', map.worldWidth*2, map.worldHeight*2, 16, 16);
+    collisionLayerObj.callbacks = []; //necessary I guess, probably should bug report this
 
     var lowerLayer = [];
-    var collisionLayer = [];
 
     (function() {
       for(var x = 0; x < map.worldWidth*2; x++) {
         lowerLayer[x] = [];
-        collisionLayer[x] = [];
       }
     })();
 
     (function() {
       for(var x = 0; x < map.worldWidth*2; x+=2) {
         for(var y = 0; y < map.worldHeight*2; y+=2) {
-          lowerLayer[x][y]    = new Phaser.Tile('terrain', 6, x, y, 16, 16);
-          lowerLayer[x][y+1]  = new Phaser.Tile('terrain', 7, x, y+1, 16, 16);
-          lowerLayer[x+1][y]  = new Phaser.Tile('terrain', 6+32, x+1, y, 16, 16);
-          lowerLayer[x+1][y+1]= new Phaser.Tile('terrain', 7+32, x+1, y+1, 16, 16);
+          lowerLayer[y][x]    = new Phaser.Tile('terrain', 6, x, y, 16, 16);
+          lowerLayer[y][x+1]  = new Phaser.Tile('terrain', 7, x+1, y, 16, 16);
+          lowerLayer[y+1][x]  = new Phaser.Tile('terrain', 6+32, x, y+1, 16, 16);
+          lowerLayer[y+1][x+1]= new Phaser.Tile('terrain', 7+32, x+1, y+1, 16, 16);
         }
       }
     })();
@@ -90,22 +98,15 @@ Game.prototype = {
     for(var x = 0; x < map.worldWidth*2; x+=2) {
       for(var y = 0; y < map.worldHeight*2; y+=2) {
 
-        switch(map.world[x/2][y/2]) {
+        var hx = x/2;
+        var hy = y/2;
+
+        switch(map.world[hx][hy]) {
           case 4:
-            collisionLayer[x][y]    = new Phaser.Tile(collisionLayerObj, 768+30, x, y, 16, 16);
-            collisionLayer[x][y+1]  = new Phaser.Tile(collisionLayerObj, 768+31, x, y+1, 16, 16);
-            collisionLayer[x+1][y]  = new Phaser.Tile(collisionLayerObj, 768+30+32, x+1, y, 16, 16);
-            collisionLayer[x+1][y+1]= new Phaser.Tile(collisionLayerObj, 768+31+32, x+1, y+1, 16, 16);
-
-            collisionLayer[x][y].setCollision(true, true, true, true);
-            collisionLayer[x][y+1].setCollision(true, true, true, true);
-            collisionLayer[x+1][y].setCollision(true, true, true, true);
-            collisionLayer[x+1][y+1].setCollision(true, true, true, true);
-
-            //tilemap.putTile(768+30, x, y, 'collision');
-            //tilemap.putTile(768+31, x+1, y, 'collision');
-            //tilemap.putTile(768+30+32, x, y+1, 'collision');
-            //tilemap.putTile(768+31+32, x+1, y+1, 'collision');
+            tilemap.layers[1].data[y][x] = new Phaser.Tile(collisionLayerObj, 768 + 30, x, y, 16, 16);
+            tilemap.layers[1].data[y][x + 1] = new Phaser.Tile(collisionLayerObj, 768 + 31, x + 1, y, 16, 16);
+            tilemap.layers[1].data[y + 1][x] = new Phaser.Tile(collisionLayerObj, 768 + 30 + 32, x, y + 1, 16, 16);
+            tilemap.layers[1].data[y + 1][x + 1] = new Phaser.Tile(collisionLayerObj, 768 + 31 + 32, x + 1, y + 1, 16, 16);
             break;
 
           default:
@@ -117,10 +118,38 @@ Game.prototype = {
       }
     }
 
-    tilemap.layers[1].data = collisionLayer;
+    //TODO possibly, calculate all changes and store in a separate 2d array
+    //then apply them with another loop
+    //yay loops
+    (function() {
+      for(var x = 0; x < map.worldWidth*2; x+=2) {
+        for (var y = 0; y < map.worldHeight * 2; y += 2) {
 
-    //causes an error!
-    //tilemap.setCollision([768+30, 768+31, 768+30+32, 768+31+32], true, 'collision', true);
+          switch (map.world[x / 2][y / 2]) {
+            case 4:
+
+              //var start = (map.world[hx][hy+1] === 4) * 1 +
+              //    (map.world[hx+1][hy] === 4) * 2 +
+              //    (map.world[hx][hy-1] === 4) * 4 +
+              //    (map.world[hx-1][hy] === 4) * 8;
+
+              //tilemap.layers[1].data[y][x].index = 100;
+              //tilemap.layers[1].data[y][x+1].index = 100;
+              //tilemap.layers[1].data[y+1][x].index = 100;
+              //tilemap.layers[1].data[y+1][x+1].index = 100;
+              break;
+
+            default:
+              spawnx = x / 2;
+              spawny = y / 2;
+              break;
+          }
+
+        }
+      }
+    })();
+
+    tilemap.setCollision([768+30, 768+31, 768+30+32, 768+31+32], true, 'collision');
 
     this.testentity = new Player(this.game, spawnx*32, spawny*32, collisionLayerObj);
 
